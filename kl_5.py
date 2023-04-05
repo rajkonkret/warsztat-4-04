aa79168aacfc988ae5c461fd802c99b28bd31e59
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-04-05 12:14:15 +0200

    Add files via upload
    add kl_6

--- kl_5.py
+++ kl_5.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Counter(ABC):
@@ -12,6 +12,14 @@ class Counter(ABC):
     def format_string():
         return '%d'
 
+    @abstractmethod
+    def drukuj(self):
+        pass
+
+    @classmethod
+    def from_other(cls, counter):
+        return cls(counter.values)
+
 
 class BoundedCounter(Counter):
     MAX = 100
@@ -21,14 +29,27 @@ class BoundedCounter(Counter):
             raise ValueError("Wartosć nie może przekroczyc MAX")
         super(BoundedCounter, self).__init__(values)
 
+    def increment(self, by=1):
+        super(BoundedCounter, self).increment()
+        self.values = min(self.values, self.MAX)  # min() - wartosc minimalna
 
-c = Counter()
-c.increment()
-print(c)
-c.format_string()
+    def drukuj(self):
+        print("Drukuje....", self.values)
+
+
+# c = Counter()
+# c.increment()
+# print(c)
+# c.format_string()
 # Metoda statyczna - metoda ktora mozna wywołac na klasie a nie na obiekcie klasy
 Counter.format_string()
-print(Counter().format_string())
 b = BoundedCounter()
 # b2 = BoundedCounter(101) - rzuci nam wyjątek
 # 11:30
+# klasa abstrakcyjna - nie mozna utworzyc obiektu takiej klasy
+# klasy dziedziczace musza nadpisac metody abstrakcyjne
+b.increment(10)
+b.drukuj()
+c = BoundedCounter.from_other(b)
+b.drukuj()
+c.drukuj()
